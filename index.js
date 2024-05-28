9707e66ee79fab4f6b0edacd9ea240a6b2d52e5c
ayon008 <shariar.ayon128@gmail.com>
2024-05-29 02:47:40 +0600

    fff

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 
 const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+};
 const app = express();
 const port = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static("public"));
