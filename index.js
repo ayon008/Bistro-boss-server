9dc8ebc2cff6a91039401201a9582b478a1e5949
ayon008 <shariar.ayon128@gmail.com>
2025-06-08 13:19:35 +0600

    a

--- index.js
+++ index.js
@@ -21,6 +21,29 @@ app.use(express.static("public"));
 
 
 
+const allowedOrigins = [
+    'https://bistro-boss-final-ea0a9.web.app/',
+    'https://www.bistro-boss-final-ea0a9.web.app/'
+];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
+
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
